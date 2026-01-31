603b2211cf7ed91a2fc49646cf78a0965ddb612b
Emmeline <emmathemartian@gmail.com>
2026-01-30 21:23:27 -0600

    Build

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
-gcc -orobots -Iraylib/src/ src/main.c ./raylib/src/libraylib.a -lm
+os=$(uname)
+if [ "$os" = "Linux" ]
+then
+	gcc -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 \
+		-o robots -Iraylib/src/ src/main.c ./raylib/src/libraylib.a
+elif [ "$os" = "Darwin" ]
+then
+	clang -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL \
+		-o robots -Iraylib/src/ src/main.c ./raylib/src/libraylib.a
+fi
+
 ./robots
